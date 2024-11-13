e9eaeb76a71ef3b0ea8d10aacb1fd7684650d36d
MoQiSz <animevanguards1yrsz@gmail.com>
2024-11-13 09:03:42 +0700

    Update load.lua

--- Gui/load.lua
+++ Gui/load.lua
@@ -22,58 +22,53 @@ local Loading = {} do
                     end
                 end
             )
+            Loader = main:Notify(
+                {
+                    Title = "[Loading]"..main.Name,
+                    SubContent = "0% / 100%",
+                    Disable = true
+                }
+            )
             task.spawn(
                 function()
-                    Loader = main:Notify(
-                        {
-                            Title = "[Loading]"..main.Name,
-                            SubContent = "0% / 100%",
-                            Disable = true,
-                            Duration = function()
-                                repeat
-                                    main.NotifyHolder.Parent = NotifyHolder
+                    while true and task.wait() do
+                        if not main.GUI.Parent then
+                            break
+                        elseif Loader then
+                            main.NotifyHolder.Parent = NotifyHolder
 
-                                    if main.GUI and main.GUI.Enabled then
-                                        main.GUI.Enabled = false
-                                    end
-
-                                    if not path then
-                                        Loader.SubContentLabel.Text = (main.Loaded >= 100 and "100") or tostring(main.Loaded).."% / 100%"
-
-                                        if main.Loaded >= 100 then
-                                            if Connect.Connected then
-                                                Connect:Disconnect()
-                                            end
-                                            task.wait(0.55)
-                                            break
-                                        else
-                                            main.Loaded += 1
-                                        end
-                                    end
-
-                                    pcall(
-                                        function()
-                                            Loader.SubContentLabel.Text = (main.Loaded >= 100 and "100") or tostring(main.Loaded).."% / 100%"
-                                        end
-                                    )
-
-                                    task.wait()
-                                until not main.GUI.Parent
-                                task.spawn(
-                                    function()
-                                        if main.GUI.Parent then
-                                            main.GUI.Enabled = true
-                                            main.NotifyHolder.Parent = main.GUI
-                                            NotifyHolder:Destroy()
-                                            if Connect.Connected then
-                                                Connect:Disconnect()
-                                            end
-                                        end
-                                    end
-                                )
+                            if main.GUI and main.GUI.Enabled then
+                                main.GUI.Enabled = false
                             end
-                        }
-                    )
+
+                            pcall(
+                                function()
+                                    Loader.SubContentLabel.Text = (main.Loaded >= 100 and "100") or tostring(main.Loaded).."% / 100%"
+                                end
+                            )
+
+                            if not path then
+                                Loader.SubContentLabel.Text = (main.Loaded >= 100 and "100") or tostring(main.Loaded).."% / 100%"
+
+                                if main.Loaded >= 100 then
+                                    if main.GUI.Parent then
+                                        main.GUI.Enabled = true
+                                        main.NotifyHolder.Parent = main.GUI
+                                        NotifyHolder:Destroy()
+                                        if Connect.Connected then
+                                            Connect:Disconnect()
+                                        end
+                                        Loader:Close()
+                                    end
+                                    task.wait(0.55)
+                                    break
+                                else
+                                    main.Loaded += 1
+                                end
+                            end
+
+                        end
+                    end
                 end
             )
         else
